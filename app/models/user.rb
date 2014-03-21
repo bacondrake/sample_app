61242b7e0ee5cceddb4728f4a67b488084810b5f
bacondrake <frank.pavey@gmail.com>
2014-03-21 23:09:37 +1100

    Make a basic User model (including secure passwords)

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
+
+
+#regular expressions (regex):
+#as in the above valid_email_regex
+=begin
+/            #start of regex
+\A           #match start of a string
+[\w+\-.]+    #at least one (w)ord character, plus, hyphen or dot
+@            #literal "at sign"
+[a-z\d\-.]+  #at least one letter, (d)igit, hyphen, or dot
+\.           #literal dot
+[a-z]+       #at least one letter
+\z           #match end of a string
+/            #end of regex
+i            #case insensitive
+
+#Rubular can test regex - figure 6.4 in Hartl Rails Tutorial
+=end
--- spec/helpers/static_pages_helper_spec.rb
+++ spec/helpers/static_pages_helper_spec.rb
@@ -10,6 +10,24 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe StaticPagesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+#describe StaticPagesHelper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+describe ApplicationHelper do
+
+  describe "full_title" do
+    it "should include the page title" do
+      expect(full_title("foo")).to match(/foo/)
+    end
+
+    it "should include the base title" do
+      expect(full_title("foo")).to match(/^Ruby on Rails Tutorial Sample App/)
+    end
+
+    it "should not include a bar for the home page" do
+      expect(full_title("")).not_to match(/\|/)
+    end
+  end
 end
+
